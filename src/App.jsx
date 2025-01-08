e8ef7a70cbb8e6e29c69865b41f62195ddc42481
Surya Annadurai <suryaannadurai1041999@gmail.com>
2025-01-08 21:52:25 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Nav from './Components/Nav/NavSection'
+import Nav from './Components/Nav/NavSection.jsx'
 import Search from './Components/Search/SearchSection'
-
 import Body from './Components/Body/BodySection.'
 
 function App() {
